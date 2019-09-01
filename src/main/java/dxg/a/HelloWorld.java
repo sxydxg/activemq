7caa8d29950ec20be99ce784a4865a9d11e911a0
dxg2 <dxg_sys@163.com>
2019-09-01 18:39:10 +0800

    Hello World!

---
+++ src/main/java/dxg/a/HelloWorld.java
@@ -0,0 +1,11 @@
+package dxg.a;
+
+/**
+ * @auther 丁溪贵
+ * @date 2019/9/1
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World！");
+    }
+}
